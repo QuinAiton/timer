cf238d21264dc4e2ce91bcf625720842924bb814
QuintenAiton <quinaiton@hotmail.com>
2021-01-12 22:02:12 +0000

    ran module through eslint for formatting

--- timer.js
+++ timer.js
@@ -1,14 +1,14 @@
-const args = process.argv.slice(2)
+const args = process.argv.slice(2);
 const timer = (args) => {
   for (const time of args) {
     let number = Number(time);
     if (time > 0) {
       setTimeout(() => {
-        process.stdout.write('beeep')
-        process.stdout.write('\n')
-      }, number * 1000)
+        process.stdout.write('beeep');
+        process.stdout.write('\n');
+      }, number * 1000);
     }
   }
-}
+};
 
 timer(args);
